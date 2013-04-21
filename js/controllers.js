add43cdecea1d03d576432958f3abf446337e907
Mark Smith-Guerrero <msmithgu@gmail.com>
2013-04-21 13:20:42 -0400

    added a bit more sample data

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,8 @@
 function VolunteerCtrl($scope) {
   $scope.volunteers = [
-  {name: 'Ann'}];
+  { name: 'Ann Margaret' , email: 'ann@example.net' },
+  { name: 'Margaret Ann', email: 'margaret@example.net' }
+  ];
 
   $scope.addVolunteer = function() {
     $scope.volunteers.unshift({name:$scope.volunteerName});
