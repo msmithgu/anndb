ca39e8ed08524a986860f7726313e0e7b08de7ea
Mark Smith-Guerrero <msmithgu@gmail.com>
2013-04-21 15:44:00 -0400

    added more fields to sample data

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,22 @@
 function VolunteerCtrl($scope) {
   $scope.volunteers = [
-  { name: 'Ann Margaret' , email: 'ann@example.net' },
-  { name: 'Margaret Ann', email: 'margaret@example.net' }
+    { name: 'Ann Margaret'
+    , email: 'ann@example.net'
+    , comment: 'Gets things done.'
+    , email: 'alice@example.com'
+    , address1: '975 Quaker Street'
+    , address2: 'Alexandria, VA 22304'
+    , phone: '555.555.3214'
+    , member: true
+    },
+    { name: 'Margaret Ann'
+    , email: 'margaret@example.net'
+    , comment: 'Talks.. A lot.'
+    , address1: '123 Main Street'
+    , address2: 'Manassass, VA'
+    , phone: '555.555.3214'
+    , member: true
+    }
   ];
 
   $scope.addVolunteer = function() {
